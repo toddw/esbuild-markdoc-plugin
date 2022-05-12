7371189f491f51d6e632fea4d025f10e09090835
Todd W <toddsurfs@icloud.com>
2022-05-11 20:51:17 -0700

    migrate from ts imports to js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import fs from 'fs';
-import Markdoc from '@markdoc/markdoc';
+const fs = require("fs");
+const Markdoc = require("@markdoc/markdoc");
 
-export default function markdocPlugin() {
+function markdocPlugin() {
   return {
-    name: 'markdoc',
+    name: "markdoc",
     setup(build) {
-      build.onLoad({filter: /\.md$/}, async args => {
-        const data = await fs.promises.readFile(args.path, 'utf8');
+      build.onLoad({ filter: /\.md$/ }, async args => {
+        const data = await fs.promises.readFile(args.path, "utf8");
         const ast = Markdoc.parse(data);
         const content = Markdoc.transform(ast);
         return {
           contents: JSON.stringify(content),
-          loader: 'json',
+          loader: "json"
         };
       });
-    },
+    }
   };
 }
+
+module.exports = markdocPlugin;
