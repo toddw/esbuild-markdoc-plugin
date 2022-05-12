2e3c8cb6f1c6a70b101bf806d19d220d00e5eb0a
Todd W <toddsurfs@icloud.com>
2022-05-11 21:03:03 -0700

    support config

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-const fs = require("fs");
-const Markdoc = require("@markdoc/markdoc");
+const fs = require('fs');
+const Markdoc = require('@markdoc/markdoc');
 
-function markdocPlugin() {
+function markdocPlugin(config = {}) {
   return {
-    name: "markdoc",
+    name: 'markdoc',
     setup(build) {
-      build.onLoad({ filter: /\.md$/ }, async args => {
-        const data = await fs.promises.readFile(args.path, "utf8");
+      build.onLoad({filter: /\.md$/}, async args => {
+        const data = await fs.promises.readFile(args.path, 'utf8');
         const ast = Markdoc.parse(data);
-        const content = Markdoc.transform(ast);
+        const content = Markdoc.transform(ast, config);
         return {
           contents: JSON.stringify(content),
-          loader: "json"
+          loader: 'json',
         };
       });
-    }
+    },
   };
 }
 
